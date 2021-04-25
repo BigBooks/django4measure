4bb8f58d034698710dc9deb90c553b0643bde186
hasson <hassonl@foxmail.com>
2021-04-25 14:52:46 +0800

    传错了  尴尬

--- measure_tensorflow/views.py
+++ measure_tensorflow/views.py
@@ -9,6 +9,7 @@ import skimage.transform as trans
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 import glob
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
@@ -20,29 +21,52 @@ print("load successful")
 config = ConfigProto()
 config.gpu_options.allow_growth = True  # GPU内存增长
 session = InteractiveSession(config=config)
+
 img = io.imread("./0.png", as_gray=True)
 img = img / 255
 img = trans.resize(img, (256, 256))
 img = np.reshape(img, img.shape + (1,)) if (not False) else img
 img = np.reshape(img, (1,) + img.shape)
 result = model.predict(img, verbose=1)
-##提前加载好，以后预测的时候就速度快了
+#提前加载好，以后预测的时候就速度快了
+
 @csrf_exempt
 def slot(request):
-    # pic = request.FILES.get('pic')
-    img = io.imread("./test.png", as_gray=True)
+    isVertical = request.POST['isVertical']
+    print(isVertical)
+    # return HttpResponse(2)
+
+    img = io.imread(os.path.join("~/Desktop","test.png"), as_gray=True)
     img = img / 255
     img = trans.resize(img, (256, 256))
     img = np.reshape(img, img.shape + (1,)) if (not False) else img
     img = np.reshape(img, (1,) + img.shape)
     result = model.predict(img, verbose=1)
-    result = result[0, :, :, :]
-    result = result[:,:,0]
-    # img = labelVisualize(result, COLOR_DICT,
-    #                      result) if False else result[:, :, 0]
-    print(result.shape)
-    # io.imsave("./result.png",img)
-    return HttpResponse(img)
+    result = result[0, :, :, 0]
+    axis = 1
+    if(isVertical == True):
+        axis =0
+    colSum = np.sum(result,axis=axis)
+    max = np.max(colSum)
+    min = np.min(colSum)
+    delta = max - min
+    colSum = (colSum - min)/delta
+    smallIndex = []
+    for i in range(256):
+        if colSum[i] <=0.6:
+            smallIndex.append(i)
+    indexMean = np.mean(smallIndex)
+    c1 = -1
+    c2 = -1
+    if(len(smallIndex)>1):
+        c1 = int(np.mean([x for x in smallIndex if x < indexMean]))*2
+        c2 = int(np.mean([x for x in smallIndex if x > indexMean]))*2
+    response  = []
+    dict = {}
+    dict['index1'] = c1
+    dict['index2'] = c2
+    response.append(dict)
+    return HttpResponse(response,content_type="application/json")
 
 
 # def params_post(request):
---
+++ readme.md
@@ -0,0 +1,4 @@
+1. 安装django 及unet项目的相关环境https://github.com/nothasson/unet ,其他的话，看看输出的错误哈哈
+2.  views.py修改模型文件路径等
+3.  命令行输入 python .\manage.py runserver(好像还有一步，忘记了，但是出错的话 命令行会提醒你该输入什么的)
+4. 
